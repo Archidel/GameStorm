ae3af9a6c72176183791ad9cb6ae475bddd25c02
Archidel <archangel.albert.6@gmail.com>
2016-12-02 21:11:40 +0300

    add methods equalse, hashcod, toString in beans

--- GameStorm/src/by/htp6/store/bean/Game.java
+++ GameStorm/src/by/htp6/store/bean/Game.java
@@ -90,4 +90,91 @@ public class Game implements Serializable{
 	public void setGameplay(String gameplay) {
 		this.gameplay = gameplay;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dataRelease == null) ? 0 : dataRelease.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((developer == null) ? 0 : developer.hashCode());
+		result = prime * result + ((gameplay == null) ? 0 : gameplay.hashCode());
+		result = prime * result + ((ganre == null) ? 0 : ganre.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((image == null) ? 0 : image.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((partOfseries == null) ? 0 : partOfseries.hashCode());
+		result = prime * result + price;
+		result = prime * result + ((site == null) ? 0 : site.hashCode());
+		result = prime * result + (status ? 1231 : 1237);
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		if (dataRelease == null) {
+			if (other.dataRelease != null)
+				return false;
+		} else if (!dataRelease.equals(other.dataRelease))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (developer == null) {
+			if (other.developer != null)
+				return false;
+		} else if (!developer.equals(other.developer))
+			return false;
+		if (gameplay == null) {
+			if (other.gameplay != null)
+				return false;
+		} else if (!gameplay.equals(other.gameplay))
+			return false;
+		if (ganre == null) {
+			if (other.ganre != null)
+				return false;
+		} else if (!ganre.equals(other.ganre))
+			return false;
+		if (id != other.id)
+			return false;
+		if (image == null) {
+			if (other.image != null)
+				return false;
+		} else if (!image.equals(other.image))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (partOfseries == null) {
+			if (other.partOfseries != null)
+				return false;
+		} else if (!partOfseries.equals(other.partOfseries))
+			return false;
+		if (price != other.price)
+			return false;
+		if (site == null) {
+			if (other.site != null)
+				return false;
+		} else if (!site.equals(other.site))
+			return false;
+		if (status != other.status)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Game [id=" + id + ", name=" + name + ", price=" + price + ", developer=" + developer + ", dataRelease="
+				+ dataRelease + ", partOfseries=" + partOfseries + ", ganre=" + ganre + ", image=" + image + ", site="
+				+ site + ", status=" + status + ", description=" + description + ", gameplay=" + gameplay + "]";
+	}
+
 }
--- GameStorm/src/by/htp6/store/bean/User.java
+++ GameStorm/src/by/htp6/store/bean/User.java
@@ -85,4 +85,75 @@ public class User implements Serializable{
 	public void setId(int id) {
 		this.id = id;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + accessLevel;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + (status ? 1231 : 1237);
+		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+		result = prime * result + ((yearsOld == null) ? 0 : yearsOld.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (accessLevel != other.accessLevel)
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id != other.id)
+			return false;
+		if (login == null) {
+			if (other.login != null)
+				return false;
+		} else if (!login.equals(other.login))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (status != other.status)
+			return false;
+		if (surname == null) {
+			if (other.surname != null)
+				return false;
+		} else if (!surname.equals(other.surname))
+			return false;
+		if (yearsOld == null) {
+			if (other.yearsOld != null)
+				return false;
+		} else if (!yearsOld.equals(other.yearsOld))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", accessLevel=" + accessLevel + ", name=" + name + ", surname=" + surname
+				+ ", yearsOld=" + yearsOld + ", email=" + email + ", password=" + password + ", login=" + login
+				+ ", status=" + status + "]";
+	}
 }
